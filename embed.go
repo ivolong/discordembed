596586938fe62575f69ed4648765294d128b8bfa
Ivo <36451843+ivolong@users.noreply.github.com>
2019-12-02 00:14:28 +0000

    Update embed.go

--- embed.go
+++ embed.go
@@ -3,20 +3,43 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 	"net/http"
 	"bytes"
 )
 
 func main () {
-  jsonString := []byte(`{"content": "not finished"}`)
-  post, error := http.NewRequest("POST", "https://discordapp.com/api/webhooks/", bytes.NewBuffer(jsonString))
-  post.Header.Set("Content-type", "application/json")
+	data := []byte(`{
+		"content": "Yes",
+		"embeds": [
+			{
+				"type": "rich",
+				"title": "Yes",
+				"description": "Yes",
+				"fields": [
+					{
+						"name": "Yes",
+						"inline": true,
+						"value": "Yes"
+					}, {
+						"name": "Coins",
+						"inline": true,
+						"value": "Yes"
+					}
+				],
+				"thumbnail": {
+					"url": "Yes"
+				}
+			}
+		]
+	}`)
+	post, error := http.NewRequest("POST", "https://discordapp.com/api/webhooks/", bytes.NewBuffer(jsonString))
+	post.Header.Set("Content-type", "application/json")
 
-  client := &http.Client{}
-  response, error := client.Do(post)
-  if error != nil {
-    fmt.Println("HTTP error when posting to Discord")
-  }
-  defer response.Body.Close()
+	client := &http.Client{}
+	response, error := client.Do(post)
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer response.Body.Close()
 }
